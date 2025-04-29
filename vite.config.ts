3c60f6e174409ad068e316787fc4f4c5263f936f
Jovenjr <samueljuniormartinez@gmail.com>
2025-04-29 16:37:40 -0400

    Set base to root for Cloudflare Pages

--- vite.config.ts
+++ vite.config.ts
@@ -2,9 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
-export default defineConfig(({ command }) => {
-  return {
-    plugins: [react()],
-    base: command === 'build' ? '/DiapositivaReact-/' : '/',
-  };
+export default defineConfig({
+  plugins: [react()],
+  base: '/',
 })
